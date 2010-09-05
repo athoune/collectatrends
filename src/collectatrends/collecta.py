1aea1c8c1728d7e045fa118b19a7a42facd84537
Mathieu Lecarme <mathieu@garambrogne.net>
2010-09-05 18:28:44 +0200

    basic entry

--- src/collectatrends/collecta.py
+++ src/collectatrends/collecta.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 	c = Collecta()
 	results = c.query('python')
 	print len(results)
-	#for result in results:
-		#pass
-	#	print ' '
-	#	print result.tag
+	#print list(results.keys())
+	for result in results:
+		print result.id
+		print "\t", result.title
--- src/collectatrends/opensearch.py
+++ src/collectatrends/opensearch.py
@@ -16,12 +16,18 @@ class Feed(object):
 		refresh_url
 		results
 		"""
+	def keys(self):
+		for entry in self.tree.getiterator('*'):
+			yield entry.tag
 	def __len__(self):
 		return int(self.tree.find('{http://a9.com/-/spec/opensearch/1.1/}totalResults').text)
 	def __iter__(self):
-		for entry in self.tree.getiterator():#'{http://www.w3.org/2005/Atom}entry'):
-			yield entry
+		for entry in self.tree.getiterator('{http://www.w3.org/2005/Atom}entry'):
+			yield Entry(entry)
 	
 class Entry(object):
 	def __init__(self, raw):
-		pass
+		self.raw = raw
+		self.id = raw.find('{http://www.w3.org/2005/Atom}id').text
+		self.title = raw.find('{http://www.w3.org/2005/Atom}title').text
+		self.link = raw.find('{http://www.w3.org/2005/Atom}link').text
